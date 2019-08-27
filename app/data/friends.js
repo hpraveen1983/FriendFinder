ab8a39a99ae10bc48314cfa8d3182ba3b7d46ff9
hpraveen <hariharanster@gmail.com>
2019-08-26 22:34:55 -0400

    created friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendsList = [
+	{
+	  name: "callie",
+	  photo: "http://callieburrito.com/wp-content/uploads/2016/10/IMG_3800-500x310.jpg",
+	  preferences: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
+	},
+
+	{
+	  name: "callie burrito",
+	  photo: "http://callieburrito.com/wp-content/uploads/2016/11/cropped-test.png",
+	  preferences: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
+	},
+
+	{
+	  name: "twitter bird",
+	  photo: "http://callieburrito.com/wp-content/uploads/2016/10/IMG_0566-500x310.jpg",
+	  preferences: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+	},
+];
